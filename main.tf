ffe8f4246c9023de5cd45bd32702b81f2c2ae342
David Ko <kdj890821@gmail.com>
2024-03-31 17:25:56 -0400

    Code fix to satisfy discrepancies in newer module versions

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "blog_autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
+  target_group_arns   = [module.blog_alb.arn]
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
@@ -66,7 +66,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
+      # target_id        = aws_instance.blog.id
     }
   }
 
