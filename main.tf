aa5ad645d7319874662d5ebc850507da2a53ba93
David Ko <kdj890821@gmail.com>
2024-03-31 17:42:43 -0400

    Fixed typo in workaround

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "blog_autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = [for k, v in module.alb.target_groups : v.arn]
+  target_group_arns   = [for k, v in module.blog_alb.target_groups : v.arn]
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
