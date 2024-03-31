02d4c0a06b0c9b075a1bbe8cd86fdafa640698a6
David Ko <kdj890821@gmail.com>
2024-03-31 17:02:43 -0400

    Fixed target group and listeners

--- main.tf
+++ main.tf
@@ -61,19 +61,16 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
   }
 
   listeners = {
     ex-http = {
       port            = 80
-      protocol        = "HTTPS"
+      protocol        = "HTTP"
+      forward = {
+        target_group_key = "ex-instance"
+      }
     }
   }
 
